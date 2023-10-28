1e4739ac34c3c7f64731fa8dce6d1a2f39f845fe
EagleBlood <35221@student.pwsztar.edu.pl>
2023-10-28 17:21:09 +0200

    road added

--- Canvas.java
+++ Canvas.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Canvas {
     private Dot dot;
     private List<Lamp> lamps;
+    private Color pathColor = Color.GRAY;
+    private int pathThickness = 30; // Adjust the thickness as needed
 
     public Canvas(Dot dot) {
         this.dot = dot;
@@ -13,12 +15,18 @@ public class Canvas {
         // Add lamps in each corner of the path
         lamps.add(new Lamp(50, 90)); // Top left corner
         lamps.add(new Lamp(250, 90)); // Top right corner
-        lamps.add(new Lamp(250, 300)); // Bottom right corner
-        lamps.add(new Lamp(50, 300)); // Bottom left corner
+        lamps.add(new Lamp(250, 310)); // Bottom right corner
+        lamps.add(new Lamp(50, 310)); // Bottom left corner
     }
 
     public void draw(Graphics g) {
         // Clear the canvas or background, if needed
+        // Draw the path with a thicker line
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(pathColor);
+        g2d.setStroke(new BasicStroke(pathThickness));
+        g2d.drawRect(50, 100, 200, 200); // Adjust the coordinates and size as needed
+
         // Draw other elements, if any
         if (dot != null) {
             dot.draw(g);
